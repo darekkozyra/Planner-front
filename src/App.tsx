c5fd718b31d890f0e60ee3677c05f9cb0c482cc5
Darek Kozyra <darosg1@gmail.com>
2023-03-19 13:49:19 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {TestTest} from 'types';
+
+function App() {
+  const foobar: TestTest = {
+    x: 123,
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
